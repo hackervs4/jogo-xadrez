567b0f44c18c88057842752901f97ca893009570
Alexandre Miranda <ale.mirandamachado@gmail.com>
2016-10-03 12:10:35 -0300

    Fazendo os primeiros testes de movimentacao

--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
+
 namespace Xadrez
 {
     class Tela
@@ -22,6 +24,16 @@ namespace Xadrez
                 Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h");
+        }
+
+
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            
+            string s = Console.ReadLine();
+        char coluna = s[0];
+        int linha = int.Parse(s[1] + "");
+        return new PosicaoXadrez(coluna, linha);
         }
         public static void imprimirPeca(Peca peca)
         {
