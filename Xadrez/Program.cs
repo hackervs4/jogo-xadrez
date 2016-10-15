a8049d57d041d8645c09b7527446bf0d90d11efc
Alexandre Miranda <alexandre.mm@outlook.com>
2016-10-15 17:16:00 -0300

    Imprimindo posicoes possiveis e implementando as restrições de movimento

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using tabuleiro;
 using xadrez;   
+
 namespace Xadrez
 {
     class Program
@@ -17,25 +14,38 @@ namespace Xadrez
 
                 while (!partida.terminada)
                 {
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab);
+                    try
+                    {
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab);
+                        Console.WriteLine();
+                        Console.WriteLine("Turno " + partida.turno);
+                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
 
-                    Console.WriteLine();
-                    Console.Write("Origem: ");
-                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
 
-                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
-                    
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
 
-                    Console.Write("Destino: ");
-                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-                    partida.executaMovimento(origem, destino);
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
+
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
                 }
-                
             }
             catch (TabuleiroException e)
             {
--- Xadrez/xadrez/Torre.cs
+++ Xadrez/xadrez/Torre.cs
@@ -23,17 +23,20 @@ namespace xadrez
 
             Posicao pos = new Posicao(0, 0);
 
-            //acima
+            // acima
             pos.definirValores(posicao.linha - 1, posicao.coluna);
-            while (tab.posicaoValida(pos) && podeMover(pos)){
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
                 mat[pos.linha, pos.coluna] = true;
-                if (tab.peca(pos)!= null && tab.peca(pos).cor != cor) {
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
                     break;
                 }
                 pos.linha = pos.linha - 1;
             }
 
-            //abaixo
+
+            // abaixo
             pos.definirValores(posicao.linha + 1, posicao.coluna);
             while (tab.posicaoValida(pos) && podeMover(pos))
             {
@@ -45,8 +48,8 @@ namespace xadrez
                 pos.linha = pos.linha + 1;
             }
 
-            //direita
-            pos.definirValores(posicao.linha, posicao.coluna+1);
+            // direita
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
             while (tab.posicaoValida(pos) && podeMover(pos))
             {
                 mat[pos.linha, pos.coluna] = true;
@@ -54,11 +57,11 @@ namespace xadrez
                 {
                     break;
                 }
-                pos.linha = pos.coluna+1;
+                pos.coluna = pos.coluna + 1;
             }
 
-            //esquerda
-            pos.definirValores(posicao.linha, posicao.coluna-1);
+            // esquerda
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
             while (tab.posicaoValida(pos) && podeMover(pos))
             {
                 mat[pos.linha, pos.coluna] = true;
@@ -66,7 +69,7 @@ namespace xadrez
                 {
                     break;
                 }
-                pos.linha = pos.coluna - 1;
+                pos.coluna = pos.coluna - 1;
             }
 
 
